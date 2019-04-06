f5f8f004ebfdc7e141d6621fbf0dace3cc16d539
Shelly Huang <shellyhuang81@gmail.com>
2019-04-06 16:54:39 -0400

    make form for create account

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded())
+app.listen(port, () => console.log(`Listening on port ${port}!`))
 
 app.set('view engine', 'pug')
 app.use(express.static('static'))
@@ -9,8 +12,6 @@ app.use(express.static('static'))
 app.get('/', (req, res) => {
     res.render('home')
 }) 
-
-
 
 app.get('/login', (req, res) => {
     res.render('login')
@@ -24,8 +25,10 @@ app.get('/create-account', (req, res) => {
     res.render('create-account')
 })
 
+app.post('/create-account', (req, res, next) => {
+    console.log(req.body.username)
+    //username
+    //password
+    //email
+})
 
-
-
-
-app.listen(port, () => console.log(`Listening on port ${port}!`))
