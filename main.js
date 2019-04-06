c57f86d4f1dd2cc32cb7ea68b8e4abe3f454f727
Shelly Huang <shellyhuang81@gmail.com>
2019-04-06 17:05:46 -0400

    create group and users

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ app.listen(port, () => console.log(`Listening on port ${port}!`))
 app.set('view engine', 'pug')
 app.use(express.static('static'))
 
+//username: group
+let users = {}
+//group: people, points
+let groups = {}
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -17,18 +21,14 @@ app.get('/login', (req, res) => {
     res.render('login')
 })
 
-app.post('/login', (req, res) => {
-
-})
-
 app.get('/create-account', (req, res) => {
     res.render('create-account')
 })
 
 app.post('/create-account', (req, res, next) => {
-    console.log(req.body.username)
-    //username
-    //password
-    //email
+    //temp group until assigned
+    users[req.body.username] = null
 })
+
+
 
