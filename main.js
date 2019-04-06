7a7e5cc4e2b28ee0750130ca5cf895e0bb0b8ffc
Matthew Wong <matthew.wong@outlook.com>
2019-04-06 22:09:06 +0000

    Added test page for code submissions

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const request = require('request');
 const app = express()
 const port = 3000
 
@@ -8,12 +9,40 @@ app.use(express.static('static'))
 
 app.get('/', (req, res) => {
     res.render('home')
-}) 
+})
 
 
 
 app.get('/login', (req, res) => {
     res.render('login')
+})
+
+app.get('/code', (req, res) => {
+
+    var headers = {
+    'content-type': 'application/json'
+    };
+
+    var dataString = '{"code":"console.log(2312)", "lang":"javascript", "stdin":""}';
+        console.log('data between options')
+    var options = {
+        url: 'http://206.189.202.164:8000/compile/',
+        method: 'POST',
+        headers: headers,
+        body: dataString
+    };
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(JSON.parse(body).output);
+            res.render('code',{data: JSON.parse(body).output})
+        }
+    }
+
+    request(options, callback);
+
+
+
 })
 
 app.post('/login', (req, res) => {
