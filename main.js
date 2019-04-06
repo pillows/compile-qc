e8ce04d55c0ad61b6ca3747ab71152efabe77ad3
Shelly Huang <shellyhuang81@gmail.com>
2019-04-06 18:33:50 -0400

    send data user data to pages

--- main.js
+++ main.js
@@ -12,23 +12,37 @@ app.use(express.static('static'))
 let users = {}
 //group: people, points
 let groups = {}
+var curr_user = 'none'
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', {user: curr_user})
 }) 
 
 app.get('/login', (req, res) => {
     res.render('login')
 })
 
+app.post('/login', (req, res) => {
+    if(!users[req.body.username]){
+        res.redirect('/login')
+    }else{
+        curr_user = req.body.username
+        res.redirect('/')
+    }
+})
+
 app.get('/create-account', (req, res) => {
     res.render('create-account')
 })
 
-app.post('/create-account', (req, res, next) => {
+app.post('/create-account', (req, res) => {
     //temp group until assigned
     users[req.body.username] = null
+    curr_user = req.body.username
+
+    res.redirect('/')
 })
+
 
 
 
