d9c1d98e46065c5d4795cc79126f528ac8b0b2a9
Jack C <chen.jack@email.com>
2019-04-07 05:27:47 -0400

    Made final changes

--- static/dashboard.js
+++ static/dashboard.js
@@ -6,6 +6,24 @@ $(document).ready(() => {
         scoreBars.css('opacity', '1')
         populateLeaderboard()
     }, 1000)
+
+    const settings = {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true
+    }
+    $('.carousel-container').slick(settings)
+
+    console.log($('.assignment-card'));
+    $('.assignment-card').each(function(index){
+        console.log('called', $(this));
+        $(this).addClass(`card${index}`)
+        if (index === 0) {
+            $(this).click(()=>{
+                window.location.href='/code'
+            })
+        }
+    })
 })
 
 function populateLeaderboard () {
